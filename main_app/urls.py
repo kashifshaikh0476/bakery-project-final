c7cb81eb5fa0282dfe92081a76e86f9582b67b7e
kashif shaikh <shaikhkashif8684@gmail.com>
2026-01-06 02:49:23 +0530

    Update urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,27 +1,21 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('shop/', views.shop, name='shop'),
-    path('order/order', views.order, name='order'),
-    path('order/admin_order', views.admin_order, name='admin_order'),
-    path('order/create_order', views.create_order, name='create_order'),
-    path('order/<int:order_id>/delete/', views.delete_order, name='delete_order'),
-    path('order/<int:order_id>/edit/', views.update_order, name='update_order'),
-    path('order/<int:order_id>/box/', views.box, name='box'),
-    path('order/<int:order_id>/status/', views.status, name='status'),
-    path('order/<int:order_id>/delete_box/<int:box_id>/', views.delete_box, name='delete_box'),
-    path('accounts/signup', views.signup, name='signup'),
-
-    # Below path takes us to the details of an order:
-    path('order/<int:order_id>/', views.detail, name='detail')
+    # UPDATED: Product ID ke sath link
+    path('create_order/<int:product_id>/', views.create_order, name='create_order'),
     
+    path('order/', views.order, name='order'),
+    path('admin_order/', views.admin_order, name='admin_order'),
+    path('order/<int:order_id>/', views.detail, name='detail'),
+    path('order/<int:order_id>/delete/', views.delete_order, name='delete_order'),
+    path('order/<int:order_id>/update/', views.update_order, name='update_order'),
+    path('order/<int:order_id>/tracking/', views.order_tracker, name='order_tracker'),
+    path('accounts/signup/', views.signup, name='signup'),
+    
+    # Status update path
+    path('order/<int:order_id>/status/', views.status, name='status'),
 ]
-
-# Steps to create a new page
-# 1. Create the path in urls.py
-# 2. Create a view function
-# 3. Create the template for the page
