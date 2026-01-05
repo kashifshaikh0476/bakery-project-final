abd681db5d7623cb9ddfe8881da630cf1b64724d
kashif shaikh <shaikhkashif8684@gmail.com>
2026-01-05 15:15:33 +0530

    Update urls.py

--- honeybee/urls.py
+++ honeybee/urls.py
@@ -2,14 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views # Ye add karein
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Ye line default logout ko force-allow karegi
+    path('accounts/logout/', auth_views.LogoutView.as_view(http_method_names=['get', 'post', 'options']), name='logout'),
     path('', include('main_app.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
-# === YE LINE SABSE ZAROORI HAI ===
-# Ye batata hai ki 'media' folder kahan hai
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
